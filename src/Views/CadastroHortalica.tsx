a2697e59446c170c07699df7dae76f5c4d65fc7a
Guilherme Daniel <gui.dani13@gmail.com>
2023-06-03 16:50:11 -0300

    adicionado views iniciais

---
+++ src/Views/CadastroHortalica.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { FAB, TextInput } from "react-native-paper";
+
+export default function CadastroHortalica({
+  navigation,
+}: {
+  navigation: any;
+}) {
+  const [text, setText] = React.useState("");
+  return (
+    <View style={styles.container}>
+      <Text>Cadastro do proprietario</Text>
+      <TextInput
+        label="Email"
+        value={text}
+        style={styles.inputStyle}
+        onChangeText={(text) => setText(text)}
+      />
+      <FAB
+        icon="chevron-right"
+        style={styles.fab}
+        onPress={() => navigation.navigate("CadastroHorta")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 8,
+  },
+  inputStyle: {
+    width: "100%",
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
